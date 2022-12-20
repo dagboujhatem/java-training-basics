fa4e4858344354eb21fe36ec7d5fd35b5f85db95
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-20 23:41:01 +0100

    Les structures conditionnelles dans Java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,62 @@
 public class Main {
     public static void main(String[] args) {
-        // L'opérateur ternaire
-        final boolean isActivated = true;
-        String accountStatus = isActivated ? "Your acoount is activated" : "Your account is disabled";
-        System.out.println(accountStatus); // Output: Your acoount is activated
+        // Les structures conditionnelles
 
-        // Example 2:
+        // Example 1: if…else syntax
+        // Output: x est supérieur à y
         int x, y;
-        x = 10;
-        y = (x == 1) ? 20 : 30;
-        System.out.println("La valeur de y est:" + y); // Output: La valeur de y est:30
-        y = (x == 10) ? 20 : 30;
-        System.out.println("La valeur de y est:" + y); // Output: La valeur de y est:20
+        x = 15;
+        y = 13;
+        if (x > y) {
+            System.out.println("x est supérieur à y");
+        } else {
+            System.out.println("y est supérieur à x");
+        }
+
+        // Example 2: nested if…else syntax (if imbriqués)
+        // Output: A = 40 et B = 20
+        int a = 40;
+        int b = 20;
+        if (a == 40) {
+            if (b == 20) {
+                System.out.println("A = 40 et B = 20");
+            }
+        }
+
+        // Example 3: nested if…elseif ... else  syntax (if multiple case test)
+        // Output: L'entier 24 est divisible par 8
+        int entier;
+        entier = 24;
+        if (entier % 5 == 0 && entier % 8 == 0) {
+            System.out.println("L'entier " + entier + " est divisible par les 5 et 8");
+        } else if (entier % 8 == 0) {
+            System.out.println("L'entier " + entier + " est divisible par 8");
+        } else if (entier % 5 == 0) {
+            System.out.println("L'entier " + entier + " est divisible par 5");
+        } else {
+            System.out.println("L'entier " + entier + " est non divisible");
+        }
+
+        // Example 3: Switch/Case syntax
+        // Output: Bonjour
+        direBonjour(1);
+    }
+
+    static public void direBonjour(int choix) {
+
+        switch (choix) {
+
+            case 1:
+                System.out.println("Bonjour");
+                break;
+            case 2:
+                System.out.println("Hello");
+                break;
+
+            default:
+                System.out.println("Choix incorrect");
+                break;
+        }
 
     }
 }
