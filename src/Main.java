90c0b05d98cde9d51c363265238643e475ba507b
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-21 09:50:16 +0100

    Le casting dans Java

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Les énumérations permettent de définir un ensemble fini de constantes
+        // Le casting des types dans Java
+        // par la suite : convertion de type
+        // int to string
+        // string to int
 
-        // Example: les jours de la semaine
-        EJour today = EJour.LUNDI;
-        afficherJour(today); // Output: Lundi
 
-    }
-
-    public static void afficherJour(EJour jour) {
-        switch (jour) {
-            case LUNDI:
-                System.out.println("Lundi");
-                break;
-            case MARDI:
-                System.out.println("Mardi");
-                break;
-            case MERCREDI:
-                System.out.println("Mercredi");
-                break;
-            case JEUDI:
-                System.out.println("Jeudi");
-                break;
-            case VENDREDI:
-                System.out.println("Vendredi");
-                break;
-            case SAMEDI:
-                System.out.println("Samedi");
-                break;
-            case DIMANCHE:
-                System.out.println("Dimanche");
-                break;
-        }
     }
 }
