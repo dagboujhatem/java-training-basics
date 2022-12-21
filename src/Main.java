1de4b7d8503290c27b7acd5a9fa7524e0861c47f
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-21 10:05:48 +0100

    Le transtypage dans Java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        // Le casting des types dans Java
-        // par la suite : convertion de type
-        // int to string
-        // string to int
+        // Le transtypage de type consiste à affecter une valeur d’un type de données primitif à un autre type.
+        // En Java, il existe deux types de transtypage:
+        // 1- Transtypage implicite vers un type « plus grand » (sans perte de données):
+        //      byte => short => char => int => long => float => double
+        // 2- Transtypage explicite vers un type « plus petit » (avec perte de données):
+        //      double => float => long => int => char => short => byte
 
+        // Example 1: Transtypage implicite
+        int i = 2;
+        double d = i; // Transtypage implicite
+        System.out.println(i); // Output: 2
+        System.out.println(d); // Output: 2
 
+        // Example 2: Transtypage explicite (casting)
+        double d1 = 2.69;
+        int i1 = (int) d1;  // Transtypage explicite
+        System.out.println(d1); // Output: 2.69
+        System.out.println(i1); // Output: 2
     }
 }
