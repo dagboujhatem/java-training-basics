7b0d75a7f7abd9d15040f97c39918767d010506c
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-21 10:19:13 +0100

    La saisie clavier dans Java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Le transtypage de type consiste à affecter une valeur d’un type de données primitif à un autre type.
-        // En Java, il existe deux types de transtypage:
-        // 1- Transtypage implicite vers un type « plus grand » (sans perte de données):
-        //      byte => short => char => int => long => float => double
-        // 2- Transtypage explicite vers un type « plus petit » (avec perte de données):
-        //      double => float => long => int => char => short => byte
+        // La saisie par clavier en Java
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Donner votre nom :");
+        String fullName = sc.nextLine();
 
-        // Example 1: Transtypage implicite
-        int i = 2;
-        double d = i; // Transtypage implicite
-        System.out.println(i); // Output: 2
-        System.out.println(d); // Output: 2
+        System.out.println("Donner votre email :");
+        String email = sc.next();
 
-        // Example 2: Transtypage explicite (casting)
-        double d1 = 2.69;
-        int i1 = (int) d1;  // Transtypage explicite
-        System.out.println(d1); // Output: 2.69
-        System.out.println(i1); // Output: 2
+        System.out.println("Donner votre age :");
+        int age = sc.nextInt();
+
+        System.out.println("Veuillez saisir un mot de passe:");
+        String password = sc.nextLine();
+        System.out.println("Vous avez saisi : ");
+        System.out.println(String.format("{ fullname: %s , email : %s , age: %d, passwprd: %s }", fullName, email, age, password));
     }
 }
