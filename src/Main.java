2ac3c970dad9296efc4fc2988be2f9612808e8af
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-23 00:11:24 +0100

    le static block dans Java

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // Le fichier .jar (Java archive) est un fichier éxécutable sur n'importe quelle OS (sous un JRE).
-        // Steps dans intelIj:
-        // 1. Configuration step:
-        // Go to File menu => Project Structure => Artifacts => click ADD => Choose JAR => Choose "From modules with dependencies" =>
-        // Shoose the main class => Click "OK"
-        // 2. Create the build step: Go To Build menu => Choose "Build Artifacts"
-        // Step 3: run the artifact jar with the following command (You must be in the same directory):
-        // java -jar FILE_NAME.jar args1 args2 ... agrsN
+    static {
+        System.out.println("Run the static block.");
+    }
 
-        if (args.length == 0)
-            System.out.println("Hello world!");
-        else
-            System.out.println("hello " + args[0]);
+    public static void main(String[] args) {
+        // Les statics bloc dans Java
+        System.out.println("Hello world!");
     }
 
 
