55e43a672a02fe19e165d05f5f20a015dd2baac8
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-22 23:04:37 +0100

    L'instruction break & continue en Java

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Les structures répétitives (4 structures)
-        // 1. La boucle while
-        // 2. La boucle for
-        // 3. La boucle do…while
-        // 4. La boucle for ... each (introduit dans Java 5)
+        // L'instruction break en Java peut être utiliser comme suivant :
+        // 1. Lorsque l’instruction break est rencontrée à l’intérieur d’une boucle, la boucle est immédiatement terminée
+        //      et le contrôle de programme reprend à l’instruction suivante de la boucle.
+        // 2. Il peut être utilisé pour terminer un cas dans l’instruction switch.
 
-        // Example: La boucle while
-        int x = 1;
-        while (x < 10) {
-            System.out.print("valeur de x : " + x);
-            x++;
-            System.out.print("\n");
+        // Example 1:
+        for (int var = 100; var >= 10; var--) {
+            System.out.print("la valeur de la variable var est : " + var + "\n");
+            if (var == 99) {
+                break;
+            }
         }
-        // Output
-        // valeur de x : 1
-        // valeur de x : 2
-        // valeur de x : 3
-        // valeur de x : 4
-        // valeur de x : 5
-        // valeur de x : 6
-        // valeur de x : 7
-        // valeur de x : 8
-        // valeur de x : 9
+        System.out.print("Hors de la boucle for \n");
 
-
-        // Example: La boucle for
-
-        for (int x1 = 1; x1 < 10; x1 = x1 + 1) {
-            System.out.print("valeur de x : " + x1);
-            System.out.print("\n");
+        // Example 2
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.println("Enter a word :");
+            input = sc.nextLine();
+            if (input.equalsIgnoreCase("Next")) {
+                continue;
+            }
+            if (input.equalsIgnoreCase("OK")) {
+                break;
+            }
+            System.out.println(input);
         }
-        // Output
-        // valeur de x : 1
-        // valeur de x : 2
-        // valeur de x : 3
-        // valeur de x : 4
-        // valeur de x : 5
-        // valeur de x : 6
-        // valeur de x : 7
-        // valeur de x : 8
-        // valeur de x : 9
-
-        // Example: La boucle do…while
-        int x2 = 1;
-        do {
-            System.out.print("valeur de x : " + x2);
-            x2++;
-            System.out.print("\n");
-        } while (x2 < 10);
-        // Output
-        // valeur de x : 1
-        // valeur de x : 2
-        // valeur de x : 3
-        // valeur de x : 4
-        // valeur de x : 5
-        // valeur de x : 6
-        // valeur de x : 7
-        // valeur de x : 8
-        // valeur de x : 9
-
-        // Example: La boucle for ... each
-        // Inconvénient de ce boucle
-        // 1. On a pas l'accès à l'index du l'élémént en cours
-        // 2. Les sens du boucle est toujours de gauche à droite (de l'indice 0 à n) et y a pas de mécanisme pour boucler inversement.
-        int tab[] = {100, 105, 110, 115, 120};
-        int itemIndex = 0;
-        for (int item : tab) {
-            System.out.println(String.format("la valeur dans la case %d est %d.", itemIndex, item));
-            itemIndex++;
-        }
-        // Output
-        // la valeur dans la case 0 est 100.
-        // la valeur dans la case 1 est 105.
-        // la valeur dans la case 2 est 110.
-        // la valeur dans la case 3 est 115.
-        // la valeur dans la case 4 est 120.
     }
 
 }
