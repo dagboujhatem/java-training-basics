f25b3d449655b4798ec31b92f7b0fe1770396df1
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-22 13:43:44 +0100

    La classe Math dans Java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        // Les constates dans Java
-        // Ils sont définie par le mot clé "final"
-        final boolean isActivated = true;
-        //isActivated = false; // ERROR: cannot assign a value to final variable isActivated
-        System.out.println(isActivated);
+        // La classe Math
 
-        // une constante PI
-        final float PI = 3.14F;
-        System.out.println(PI);
+        // The round method
+        int round = Math.round(1.5F);
+        System.out.println(round);
 
+        // The ceil method
+        // La fonction Math.ceil() retourne le plus petit entier supérieur ou égal au nombre donné.
+        int ceil = (int) Math.ceil(1.2F);
+        System.out.println(ceil);
+
+        // The floor method
+        // La fonction Math.floor() retourne le plus grand entier qui est inférieur ou égal au nombre donné.
+        int floor = (int) Math.floor(1.2F);
+        System.out.println(floor);
+
+        // The max method
+        int max = Math.max(2, 5);
+        System.out.println(max);
+
+        // The min method
+        int min = Math.min(4, 7);
+        System.out.println(min);
+
+        // the random method
+        double random = Math.random(); // round between 0 and 1
+        System.out.println(random);
     }
 }
