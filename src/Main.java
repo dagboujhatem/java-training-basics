1e8efda65579a68dea4782d52fde2cdacd458d5d
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-22 23:11:55 +0100

    L'instruction break & continue en Java

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // L'instruction break en Java peut être utiliser comme suivant :
-        // 1. Lorsque l’instruction break est rencontrée à l’intérieur d’une boucle, la boucle est immédiatement terminée
-        //      et le contrôle de programme reprend à l’instruction suivante de la boucle.
-        // 2. Il peut être utilisé pour terminer un cas dans l’instruction switch.
+        // Le deduging en Java
+        System.out.println("Start counting");
+        count(10);
+        System.out.println("End counting");
+    }
 
-        // Example 1:
-        for (int var = 100; var >= 10; var--) {
-            System.out.print("la valeur de la variable var est : " + var + "\n");
-            if (var == 99) {
-                break;
-            }
-        }
-        System.out.print("Hors de la boucle for \n");
-
-        // Example 2
-        Scanner sc = new Scanner(System.in);
-        String input = "";
-        while (true) {
-            System.out.println("Enter a word :");
-            input = sc.nextLine();
-            if (input.equalsIgnoreCase("Next")) {
-                continue;
-            }
-            if (input.equalsIgnoreCase("OK")) {
-                break;
-            }
-            System.out.println(input);
+    public static void count(int limit) {
+        for (int i = 1; i < limit; i++) {
+            System.out.println(i);
         }
     }
 
