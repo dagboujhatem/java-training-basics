f9013b774b0d2d5a613a0a34553db4c680e41218
Hatem DAGHBOUJ <dagboujhatem@gmail.com>
2022-12-23 18:04:57 +0100

    création des interface dans java

--- src/EmailService.java
+++ src/EmailService.java
@@ -1,6 +1,7 @@
-public class EmailService {
+public class EmailService implements MessageService {
     private String ID;
 
+    @Override
     public void sendEmail(String message, String receiver) {
         System.out.println("Email sending with message : " + message + " , To : " + receiver);
     }
